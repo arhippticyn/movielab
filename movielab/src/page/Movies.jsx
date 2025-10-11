02f4a408ee2608b33ea88b15deada643fd347720
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-10-11 20:01:11 +0300

    Create search & end the project

--- movielab/src/page/Movies.jsx
+++ movielab/src/page/Movies.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, NavLink, useParams } from 'react-router-dom';
+import axios from 'axios';
+import styles from '../styles/Movies.module.css'
 
 const Movies = () => {
+  const [value, setValue] = useState('')
+  const [data, setData] = useState([])
+
+  const handleChange = (e) => setValue(e.target.value)
+
+  const SearchFetch = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.get(`/search/movie?include_adult=false&query=${value}&language=en-US&api_key=1bac43eb3178f898a40965000a977735`)
+      setData(response.data.results)
+    } catch {
+      console.log('error');
+    }
+  }
   return (
-    <div>Movies</div>
+    <div className={styles.movies}>
+      <form action="" onSubmit={SearchFetch} className={styles.moviesSearch}>
+        <input type="text" onChange={handleChange} value={value} />
+
+        <button type="submit">Search</button>
+      </form>
+
+      <ul>
+        {data.map((e) => {
+          return (
+            <li key={e.id}><NavLink to={`/movies/${e.id}`} className={styles.SearchLink}>{e.title}</NavLink></li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
